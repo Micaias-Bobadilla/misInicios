a63a01bc91e01cf3dc64772726f4d11273f009d9
Micaias-Bobadilla <mickyamb@gmail.com>
2022-08-29 23:35:07 -0400

    Agregado el mouseover en las 3 imagenes

--- proyecto1/js/logic.js
+++ proyecto1/js/logic.js
@@ -1,14 +1,46 @@
-const idImagen = document.getElementById("imagenTexto1")
-const idSpanImagen = document.getElementById("spanImagenTexto")
+const idImagen1= document.getElementById("imagenTexto1")
+const idSpanImagen1 = document.getElementById("spanImagenTexto1")
 
-idImagen.addEventListener("focusin", mostrarTexto)
-idImagen.addEventListener("focusout", borrarTexto)
+idImagen1.addEventListener("mouseover", mostrarTexto1)
+idImagen1.addEventListener("mouseleave", borrarTexto1)
 
-function mostrarTexto(){
+
+const idImagen2= document.getElementById("imagenTexto2")
+const idSpanImagen2 = document.getElementById("spanImagenTexto2")
+
+idImagen2.addEventListener("mouseover", mostrarTexto2)
+idImagen2.addEventListener("mouseleave", borrarTexto2)
+
+const idImagen3= document.getElementById("imagenTexto3")
+const idSpanImagen3 = document.getElementById("spanImagenTexto3")
+
+idImagen3.addEventListener("mouseover", mostrarTexto3)
+idImagen3.addEventListener("mouseleave", borrarTexto3)
+
+function mostrarTexto1(){
    
-        idSpanImagen.innerHTML = `Esto es un texto de prueba de imagen`
+        idSpanImagen1.innerHTML = "ESTO ES EL TEXTO 1"
 }
 
-function borrarTexto(){
-    idSpanImagen.innerHTML =""
+function borrarTexto1(){
+    idSpanImagen1.innerHTML =""
+}
+
+
+function mostrarTexto2(){
+   
+        idSpanImagen2.innerHTML = "THIS IS SPARTAAAA!!!!"
+}
+
+function borrarTexto2(){
+    idSpanImagen2.innerHTML =""
+}
+
+function mostrarTexto3(){
+   
+    idSpanImagen2.innerHTML = "HOLA MDAFAKA"
+}
+
+function borrarTexto3(){
+idSpanImagen2.innerHTML =""
 }
